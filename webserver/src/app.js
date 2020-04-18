a058f36a27cb4c5ce842babb6695520bbcec1c78
unknown <nentzos.pan@gmail.com>
2020-04-18 14:03:49 +1000

    Setting up webserver with Express

---
+++ webserver/src/app.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const publicDir = path.join(__dirname, "../public");
+
+app.use(express.static(publicDir));
+
+// app.com/weather
+app.get("/weather", (req, res) => {
+    res.send({
+        forecast: "Overcast",
+        location: "Melbourne",
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server is up on port 3000.");
+});
