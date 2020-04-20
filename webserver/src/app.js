f47d96d6ead5af82c098279b57bb1137558b5f96
unknown <nentzos.pan@gmail.com>
2020-04-20 13:29:49 +1000

    Changed static html with Handlebars templates

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -4,7 +4,35 @@ const express = require("express");
 const app = express();
 const publicDir = path.join(__dirname, "../public");
 
+// Set Handlebars as the View Engine / templates
+app.set("view engine", "hbs");
+// Use public folder for the static content
 app.use(express.static(publicDir));
+
+// Routes
+// app.com
+app.get("", (req, res) => {
+    res.render("index", {
+        title: "Weather App",
+        name: "Panos",
+    });
+});
+
+// app.com/about
+app.get("/about", (req, res) => {
+    res.render("about", {
+        title: "About Me",
+        name: "Panos",
+    });
+});
+
+// about.com/help
+app.get("/help", (req, res) => {
+    res.render("help", {
+        title: "Help Page",
+        helpMsg: "This is a page to provide a help message. Hjelp Hjelp."
+    })
+})
 
 // app.com/weather
 app.get("/weather", (req, res) => {
