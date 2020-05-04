61ebda89cccfef3b9f8f31ec1428de3964c5b780
panxman <nentzos.pan@gmail.com>
2020-05-04 17:23:00 +1000

    Read Endpoints - Users and Tasks

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -15,28 +15,90 @@ app.use(express.json());
 
 // Create User
 app.post("/users", (req, res) => {
-    const user = new User(req.body);
+  const user = new User(req.body);
 
-    user.save().then(() => {
-        res.status(201).send(user);
-    }).catch((e) => {
-        res.status(400).send(e);
+  user
+    .save()
+    .then(() => {
+      res.status(201).send(user);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
+// Get all users
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
+// Get 1 user
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+
+      res.send(user);
+    })
+    .catch((e) => {
+      res.status(500).send();
     });
 });
 
 // Create Task
 app.post("/tasks", (req, res) => {
-    const task = new Task(req.body);
+  const task = new Task(req.body);
 
-    task.save().then(() => {
-        res.status(201).send(task);
-    }).catch((e) => {
-        res.status(400).send(e);
+  task
+    .save()
+    .then(() => {
+      res.status(201).send(task);
     })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
+// Get all tasks
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.send(tasks);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
+// Get 1 task
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send();
+      }
+
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
 });
 
 // ~~~ End ~~~
 
 app.listen(port, () => {
-    console.log("Server is up on port: " + port);
+  console.log("Server is up on port: " + port);
 });
