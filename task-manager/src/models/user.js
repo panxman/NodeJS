d26d9f2ca8a5dda8a12c5c8d19e81f75f0de7ee6
panxman <nentzos.pan@gmail.com>
2020-05-09 15:35:54 +1000

    Add login function and make 'email' field unique

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -41,6 +42,24 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+// Check User's credentials
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("Unable to login.");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Unable to login.");
+  }
+
+  return user;
+}
+
+// Has the plaitext password before saving
 userSchema.pre("save", async function (next) {
     if (this.isModified("password")) {
         this.password = await bcrypt.hash(this.password, 8);
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -15,6 +15,17 @@ router.post("/users", async (req, res) => {
   }
 });
 
+// User Login
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 // Get all users
 router.get("/users", async (req, res) => {
   try {
