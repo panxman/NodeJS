93bb371f982d5dd8b5809156ced320b43ef957eb
panxman <nentzos.pan@gmail.com>
2020-05-18 17:13:22 +1000

    Save avatar to DB and use Sharp to resize it. Also add DELETE and GET Endpoints for avatar

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const multer = require("multer");
+const sharp = require("sharp");
 const User = require("../models/user");
 const auth = require("../middleware/authentication");
 
@@ -107,7 +108,7 @@ router.delete("/users/me", auth, async (req, res) => {
 //~~~ File uploads ~~~//
 
 const avatarUpload = multer({
-  dest: "images/avatars",
+  // dest: "avatars/",
   limits: {
     fileSize: 1000000,
   },
@@ -121,8 +122,47 @@ const avatarUpload = multer({
 });
 
 // Avatar
-router.post("/users/me/avatar", avatarUpload.single("avatar"), (req, res) => {
+router.post("/users/me/avatar", auth, avatarUpload.single("avatar"), async (req, res) => {  
+  const buffer = await sharp(req.file.buffer)
+    .resize({ width: 250, height: 250 })
+    .png()
+    .toBuffer();
+
+  req.user.avatar = buffer;
+  await req.user.save();
   res.send();
+}, (error, req, res, next) => {
+  res.status(400).send({ error: error.message });
 });
+
+// Delete Avatar
+router.delete("/users/me/avatar", auth, async (req, res) => {
+  try {
+    req.user.avatar = undefined;
+    await req.user.save();
+
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+// GET Avatar
+router.get("/users/:id/avatar", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const user = await User.findById(_id);
+
+    if (!user || !user.avatar) {
+      throw new Error();
+    }
+
+    res.set("Content-Type", "image/png");
+    res.send(user.avatar);
+  } catch (e) {
+    res.status(404).send();
+  }
+})
 
 module.exports = router;
